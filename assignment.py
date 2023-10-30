4a1a931f88663aff14f12ba0b518df67f2cba125
Jeromeg282 <113056411+Jeromeg282@users.noreply.github.com>
2023-10-30 13:30:09 -0700

    .

--- assignment.py
+++ assignment.py
@@ -22,3 +22,17 @@ retrieve a record by phone number and display all of the information
 You will need to create the table yourself. Consider what data types you will
 need to use.
 """
+
+import sqlite3
+
+file = 'dbase.db'
+connection = sqlite3.connect(file)
+print(connection)
+query = ''
+cursor=connection.cursor()
+
+cursor.execute(query)
+
+
+def decision():
+    
